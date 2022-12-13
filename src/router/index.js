cba24af2d21d86a90e485a0f4aa8c41b33863fcc
sebastien <heligon.seb@gmail.com>
2022-12-13 15:49:25 +0100

    route consultation

--- src/router/index.js
+++ src/router/index.js
@@ -57,8 +57,8 @@ const routes = [
   },
 
   {
-    path: '/collecte-fini',
-    name: 'CollectEnded',
+    path: '/consulation',
+    name: 'consultation',
     component: () => import("../views/CollecteEnded.vue"),
     children: [
 
