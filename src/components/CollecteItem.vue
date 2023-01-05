a829c8d45c9f27a28f04ba6f7468cefc3b02af6b
sebastien <heligon.seb@gmail.com>
2023-01-05 16:42:23 +0100

    mise en place du compteur de jours #22

--- src/components/CollecteItem.vue
+++ src/components/CollecteItem.vue
@@ -1,28 +1,45 @@
 <template>
+
 	<div class="d-flex align-items-center">
-	
 			<div class="me-2">
 				<UserImage :name="getPersonnelNameFromId(collecte.cible__structure__personnel_id)"></UserImage>
 			</div>
+
 			<div class="d-flex flex-column align-content-between">
 				<div class="d-flex align-items-center">
 					<small class="fw-lighter me-2">#{{collecte.id}}</small>
 					<span class="badge bg-secondary me-2">{{changeFormatDateLit(collecte.date)}}</span>
-					<span class="badge bg-warning">3 j</span>
+					<span 
+						class="badge"
+						:class="{
+							'bg-danger': colorBadge == 'danger',
+                            'bg-success': colorBadge == 'success',
+							'bg-warning': colorBadge == 'warning',
+						}"
+					>
+						<i v-if="colorBadge == 'danger'" class="bi bi-calendar-x-fill"></i>
+						<i v-else-if="colorBadge == 'warning'" class="bi bi-calendar-event-fill"></i>
+						<i v-else class="bi bi-calendar-check-fill"></i>
+						{{ remainingDays() }}
+					</span>
 				</div>
+
 				<div class="d-flex">
 					{{getPersonnelNameFromId(collecte.cible__structure__personnel_id)}}
 				</div>
+
 				<div class="d-flex">
 					<div class="fw-lighter" >
 						<i class="bi bi-check me-1" v-if="collecte.done == 'OUI'"></i>
 						{{getGroupNameFromId(collecte.information__groupe_id)}}
 					</div>
 				</div>
+
 				<div class="d-flex" v-if="collecte.projet_id">
 					<i class="bi bi-boxes"></i>
 					{{getProjetName(collecte.projet_id)}}
 				</div>
+
 				<div v-else>
 					<i class="bi bi-boxes"></i>
 					Chantier non renseigné
@@ -44,16 +61,24 @@ export default {
         collecte: Object
     },
 
+	data() {
+		return {
+            colorBadge: null
+        }
+	},
+
     computed: {
         ...mapState(['formulaires', 'listActifs', 'projetsActif']),
 
 		/**
 		 * Retourn la date de la collecte formater en d/m/Y
+		 * 
+		 * @return {string}
 		 */
 		collectDate() {
 			let date = new Date(this.collecte.date);
 			return `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
-		}
+		},
     },
 
     methods: {
@@ -117,6 +142,45 @@ export default {
 				return 'projet non renseigné'
 			}
 		},
+
+		/**
+		 * Compte le nombre de jour restant entre 2 date
+		 */
+		countRemainingDays(refDate, date = new Date) {
+			let collecteDate = new Date(refDate);
+
+			let timeDifference = collecteDate.getTime() - date.getTime();
+
+			return timeDifference;
+		},
+
+		/**
+		 * Retourne nombre de jour ou d'heure et minutes restant entre 2 date
+		 * 
+		 * @return {string}
+		 */
+		remainingDays() {
+			let countRemainingDays = this.countRemainingDays(this.collecte.date);
+
+			let totalMinutes = Math.round(countRemainingDays / (1000 * 60));
+			let days = Math.floor(totalMinutes / (60 * 24));
+			let hours = Math.floor(totalMinutes / 60);
+			let minutes = totalMinutes % 60;
+
+			if (0 == days+1){
+				this.colorBadge = 'warning';
+				return `${Math.abs(hours)}h ${Math.abs(minutes)}min`;
+			} 
+
+			this.colorBadge = 'success';
+
+			if (days < 0) {
+				this.colorBadge = 'danger';
+			}
+
+
+			return `${days+1} jour${ (days+1) > 1 || (days+1) < -1 ? "s" : ""}`;
+		}
     },
 	components: {
 		UserImage
