cdbcdfc26c412c010df7921df37e566b5b2cebb4
sebastien <heligon.seb@gmail.com>
2023-01-06 10:46:05 +0100

    Améliorer l'affichage du composant de collecte programmée sur smartphone #25

--- src/components/CollecteHeadband.vue
+++ src/components/CollecteHeadband.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="d-flex flex-row justify-content-between align-items-center">
-        <div class="d-flex align-items-center">
+    <div class="d-flex flex-wrap justify-content-between align-items-center">
+        <!-- <div class="d-flex align-items-center">
             <strong class="me-2 text-secondary" style="width:40px">#{{collecte.id}}</strong>
 
             <div>
@@ -10,7 +10,10 @@
 					</div>
 
                     <div v-else class="d-flex align-items-center">
-                        <span><user-image :name="getPersonnelNameFromId(collecte.enqueteur__structure__personnel_id)" className="me-1" size="sm" /></span>
+                        <span>
+                            <user-image :name="getPersonnelNameFromId(collecte.enqueteur__structure__personnel_id)" className="me-1" size="sm" />
+                        </span>
+                        
                         Contrôleur: {{getPersonnelNameFromId(collecte.enqueteur__structure__personnel_id)}}
                     </div>
 
@@ -33,22 +36,71 @@
             </div>
         </div>
 
-        <div class="d-flex align-items-baseline">
-            <div v-if="collecte.result_var && !editable" class="badge fs-5 text-uppercase mx-2" :class="classNameFromSAMI(collecte.result_var)">
-                {{collecte.result_var}}
+        <div class="flex-grow-1 flex-md-grow-0">
+            <div class="d-flex flex-row-reverse flex-md-row justify-content-between align-items-baseline">
+                <div v-if="collecte.result_var && collecte.result_var != 'null' && !editable" class="badge fs-5 text-uppercase mx-2 " :class="classNameFromSAMI(collecte.result_var)">
+                    {{collecte.result_var}}
+                </div>
+    
+                <div class="mb-1">
+                    <i class="bi bi-calendar-event mx-2"></i>
+                    <span class="me-2 text-warning" v-if="!collecte.date">Date non programmée</span>
+                    <span v-else class="me-2">{{changeFormatDateLit(collecte.date)}}</span>
+                </div>
+    
+            </div>
+        </div> -->
+
+        <div class="d-flex w-100">
+            <div class="d-flex align-items-center">
+                <strong class="me-2 text-secondary" style="width:40px">#{{collecte.id}}</strong>
             </div>
 
-            <div class="mb-1">
-                <i class="bi bi-calendar-event mx-2"></i>
-                <span class="me-2 text-warning" v-if="!collecte.date">Date non programmée</span>
-                <span v-else class="me-2">{{changeFormatDateLit(collecte.date)}}</span>
+            <div class="w-100 d-flex flex-column flex-md-row-reverse justify-content-md-between">
+                <div class="d-flex justify-content-between align-items-start mb-2 flex-md-row-reverse align-items-md-center">
+                    <div>
+                        <i class="bi bi-calendar-event me-2"></i>
+                        <span class="me-2 text-warning" v-if="!collecte.date">Date non programmée</span>
+                        <span v-else class="me-2">{{changeFormatDateLit(collecte.date)}}</span>
+                    </div>
+
+                    <div v-if="collecte.result_var && collecte.result_var != 'null' && !editable" class="badge fs-6 text-uppercase me-md-2" :class="classNameFromSAMI(collecte.result_var)">
+                        {{collecte.result_var}}
+                    </div>
+                </div>
+
+                <div>
+                    <div  v-if="!getPersonnelNameFromId(collecte.enqueteur__structure__personnel_id)" class="me-2 text-warning">
+						Contrôleur non programmé 
+					</div>
+
+                    <div v-else class="d-flex align-items-center">
+                        <span>
+                            <user-image :name="getPersonnelNameFromId(collecte.enqueteur__structure__personnel_id)" className="me-1" size="sm" />
+                        </span>
+                        
+                        Contrôleur: {{getPersonnelNameFromId(collecte.enqueteur__structure__personnel_id)}}
+                    </div>
+
+                    <div class="d-flex align-items-center text-secondary">
+                        <i class="bi bi-arrow-return-right me-1 ms-2"></i>
+
+                        <div v-if="!getPersonnelNameFromId(collecte.cible__structure__personnel_id)" class="me-2 text-warning">
+							Opérateur non programmé 
+						</div>
+
+                        <div v-else class="d-flex align-items-center fs-7">
+                            <span>
+								<user-image :name="getPersonnelNameFromId(collecte.cible__structure__personnel_id)" className="me-1" size="sm" />
+							</span>
+
+                            Opérateur: {{getPersonnelNameFromId(collecte.cible__structure__personnel_id)}}
+                        </div>
+                    </div>
+                </div>
             </div>
-
-			<!-- <div class="btn btn-light" v-if="editable">
-				<i class="bi bi-pencil"></i>
-			</div> -->
-
         </div>
+
     </div>
     
 </template>
