6391da9dc515a1034417dc0583a976eee37ae054
deathtroll22 <deathtroll@laposte.net>
2023-07-06 11:15:05 +0200

    Creation du composant CarteMapEt adresse

--- src/components/CarteDeControle.vue
+++ src/components/CarteDeControle.vue
@@ -27,30 +27,8 @@
 
 
 		<div class="col-12 col-lg-6 p-0 p-sm-0">
-			<div class="bg-white h-100 rounded shadow-sm">
-				<div class="card-body h-100 d-flex align-items-center justify-content-center">
-					<div class="row">
-						<div class="col-12 text-center mb-3">
-							<div class="row fw-bold">
-								<div class="col-12">CHANTIER : </div>
-								<div class="col-12">21.02.23 au 22.02.23</div>
-								<div class="col-12">RAV LORIENT</div>
-							</div>
-						</div>
-						<div class="col-12 text-center">
-							<div class="row">
-								<div class="col-12">3 rue des Pruniers</div>
-								<div class="col-12">56100 LORIENT</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-6 col-md-6 mt-2 mt-md-0">
-						<div class="d-flex justify-content-center">
-							<InteractiveMap></InteractiveMap>
-						</div>
-					</div>
-				</div>
-			</div>
+
+		<CarteMapEtAdresse></CarteMapEtAdresse>
 		</div>
 
 		<div class="text-center mt-sm-2 d-lg-none p-0">
@@ -63,12 +41,14 @@
 import SuiviHabilitation from '../components/SuiviHabilitation.vue';
 import InteractiveMap from '../components/InteractiveMap.vue';
 import MiniInfosAgent from '../components/MiniInfosAgent.vue';
+import CarteMapEtAdresse from '../components/CarteMapEtAdresse.vue';
 
 export default {
 	components: {
 		SuiviHabilitation,
 		InteractiveMap,
 		MiniInfosAgent,
+		CarteMapEtAdresse
 	},
 };
 </script>
---
+++ src/components/CarteMapEtAdresse.vue
@@ -0,0 +1,36 @@
+<template>
+
+			<div class="bg-white h-100 rounded shadow-sm">
+				<div class="card-body h-100 d-flex align-items-center justify-content-center">
+					<div class="row">
+						<div class="col-12 text-center mb-3">
+							<div class="row fw-bold">
+								<div class="col-12">RAV LORIENT</div>
+								<div class="col-12">21.02.23 au 22.02.23</div>	
+							</div>
+						</div>
+						<div class="col-12 text-center">
+							<div class="row">
+								<div class="col-12">3 rue des Pruniers</div>
+								<div class="col-12">56100 LORIENT</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-6 col-md-6 mt-2 mt-md-0">
+						<div class="d-flex justify-content-center">
+							<InteractiveMap></InteractiveMap>
+						</div>
+					</div>
+				</div>
+			</div>
+
+</template>
+<script>
+import InteractiveMap from '../components/InteractiveMap.vue';
+
+export default {
+	components: {
+	InteractiveMap,
+	},
+};
+</script>
