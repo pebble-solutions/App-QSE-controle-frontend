20586f3cc33a323dc7b50ee1eafa3bcc3aee4805
deathtroll22 <deathtroll@laposte.net>
2023-07-13 13:17:07 +0200

    idem

--- src/components/collecte/HabMonitor.vue
+++ src/components/collecte/HabMonitor.vue
@@ -1,46 +1,51 @@
 <template>
   <div class="card">
-      <div class="card-body">
-          <!-- Titre -->
-          <div class="fw-light text-secondary text-center mb-1 fw-bold">HABILITATION MONITEUR</div>
-          <div class="row">
-              <!-- Colonne 1 : Validité -->
-              <div class="col-lg-4 col-12">
-                  <div class="my-1">
-                      <div class="fw-bold col-12">Validité : 3 ans</div>
-                      <div class="col-12" v-for="hab in habilitationPerso" :key="hab.id">{{ changeFormatDateLit(hab.dd) }}
-                          au {{ changeFormatDateLit(hab.df) }}</div>
-                  </div>
-                  <!-- Composant ProgressBar -->
-                  <ProgressBar v-for="hab in habilitationPerso" :key="hab.id" :dd="hab.dd" :df="hab.df"></ProgressBar>
-              </div>
-              <!-- Colonne 2 : Résultat de groupe -->
-              <div class="col-lg-4 col-12 mt-2">
-                  <div class="d-flex align-items-center justify-content-between">
-                      
-                      <div>Dernier contrôle : {{ changeFormatDateLit(lastControlDate) }}</div>
-                  </div>
-                  <div class="d-flex flex-row-reverse align-items-center justify-content-end my-2">
-                    
-                      <button v-for="control in listControlDone" :key="control.id"
-                          :class="['btn', 'btn-sm', classNameFromSAMI(control.result_var), 'me-2', 'fs-6', 'px-2', 'text-nowrap', 'btn-square']"
-                          :data-bs-toggle="tooltip" :data-bs-placement="top" :title="'#' + control.id">
-                          {{ control.result_var }}
-                      </button>
-                  </div>
-              </div>
-              <!-- Colonne 3 : Veille -->
-              <div class="col-lg-4 col-12">
-                  <div class="my-1">
-                      <div class="fw-bold col-12">Veille : 180 jours</div>
-                      <div class="col-12" v-for="hab in habilitationPerso" :key="hab.id">{{ changeFormatDateLit(hab.dd) }}
-                          au {{ changeFormatDateLit(hab.df) }}</div>
-                  </div>
-                  <!-- Composant ProgressBar -->
-                  <ProgressBar v-for="hab in habilitationPerso" :key="hab.id" :dd="hab.dd" :df="hab.df"></ProgressBar>
-              </div>
+    <div class="card-body">
+      <!-- Titre -->
+      <div class="fw-light text-secondary text-center mb-1 fw-bold">HABILITATION MONITEUR</div>
+      <div class="row">
+        <!-- Colonne 1 : Validité -->
+        <div class="col-lg-4 col-12">
+          <div>
+            <div class="fw-bold col-12">Validité : 3 ans</div>
+            <div class="col-12 mb-1" v-for="hab in habilitationPerso" :key="hab.id">{{ changeFormatDateLit(hab.dd) }}
+              au {{ changeFormatDateLit(hab.df) }}</div>
           </div>
+          <!-- Composant ProgressBar -->
+          <ProgressBar v-for="hab in habilitationPerso" :key="hab.id" :dd="hab.dd" :df="hab.df"></ProgressBar>
+        </div>
+        <!-- Colonne 2 : Résultat de groupe -->
+        <div class="col-lg-4 col-12 mt-2">
+          <div class="d-flex align-items-center">
+
+            <!-- Ajout du nouveau bouton ici -->
+            <button
+              :class="['btn', 'btn-sm', classNameFromSAMI(resultat), 'me-2', 'fs-6', 'px-2', 'text-nowrap', 'btn-square']">
+              {{ resultat }}
+            </button>
+
+            <div>Dernier contrôle : {{ changeFormatDateLit(lastControlDate) }}</div>
+          </div>
+          <div class="d-flex flex-row-reverse align-items-center justify-content-end my-2">
+            <button v-for="control in listControlDone" :key="control.id"
+              :class="['btn', 'btn-sm', classNameFromSAMI(control.result_var), 'me-2', 'fs-6', 'px-2', 'text-nowrap', 'btn-square']"
+              :data-bs-toggle="tooltip" :data-bs-placement="top" :title="'#' + control.id">
+              {{ control.result_var }}
+            </button>
+          </div>
+        </div>
+        <!-- Colonne 3 : Veille -->
+        <div class="col-lg-4 col-12">
+          <div>
+            <div class="fw-bold col-12">Veille : 180 jours</div>
+            <div class="col-12 mb-1" v-for="hab in habilitationPerso" :key="hab.id">{{ changeFormatDateLit(hab.dd) }}
+              au {{ changeFormatDateLit(hab.df) }}</div>
+          </div>
+          <!-- Composant ProgressBar -->
+          <ProgressBar v-for="hab in habilitationPerso" :key="hab.id" :dd="hab.dd" :df="hab.df"></ProgressBar>
+        </div>
       </div>
+    </div>
   </div>
 </template>
 <script>
@@ -67,23 +72,25 @@ export default {
     loadCollecte(id) {
       this.pending.control = true;
       this.$app.apiGet('data/GET/collecte', {
-        tli : id,
+        tli: id,
         done: 'OUI'
       })
-      .then((data) => {
-        this.listControlDone = data;
-      })
-      .catch(this.$app.catchError).finally(() => this.pending.control= false);
+        .then((data) => {
+          this.listControlDone = data;
+          console.log(this.listControlDone, "ijsdfigjrsd");
+        })
+        .catch(this.$app.catchError).finally(() => this.pending.control = false);
     },
     loadHabilitation(id) {
       this.pending.control = true;
       this.$app.apiGet('v2/controle/habilitation', {
-        id : id,
+        id: id,
       })
-      .then((data) => {
-        this.habilitationPerso = data;
-      })
-      .catch(this.$app.catchError).finally(() => this.pending.control= false);
+        .then((data) => {
+          this.habilitationPerso = data;
+          console.log(this.habilitationPerso, "COUCOULES");
+        })
+        .catch(this.$app.catchError).finally(() => this.pending.control = false);
     },
     changeFormatDateLit(el) {
       return dateFormat(el);
@@ -114,6 +121,7 @@ export default {
   align-items: center;
   justify-content: center;
 }
+
 .btn {
   width: 30px;
   height: 30px;
@@ -123,6 +131,7 @@ export default {
   justify-content: center;
   position: relative;
 }
+
 .tooltip {
   position: absolute;
   top: -30px;
@@ -138,8 +147,8 @@ export default {
   visibility: hidden;
   transition: opacity 0.3s ease, visibility 0.3s ease;
 }
+
 .btn:hover .tooltip {
   opacity: 1;
   visibility: visible;
-}
-</style>
+}</style>
