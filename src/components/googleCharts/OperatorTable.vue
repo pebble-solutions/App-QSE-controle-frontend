84de2e7fcf63b1554e7df6328fdbba6e40b0c9ef
glesaout <gabin@pebble.bzh>
2023-07-21 12:10:46 +0200

    Tableau opérateur terminé

--- src/components/googleCharts/OperatorTable.vue
+++ src/components/googleCharts/OperatorTable.vue
@@ -1,128 +1,131 @@
 <template v-if="!pending.fetchData">
-  <table class="table">
-    <thead>
-      <tr>
-        <th></th>
-        <th>KN</th>
-        <th>Habilitation contrôlées</th>
-        <th>SAMI</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="(row, rowIndex) in chartData.slice(1)" :key="rowIndex">
-        <td v-for="(value, columnIndex) in row" :key="columnIndex">
-          <template v-if="columnIndex === 3">
-            <StackedBar :bars="value" :value="true"></StackedBar>
-          </template>
-          <template v-else>
-            {{ value }}
-          </template>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+	<table class="table">
+		<thead>
+			<tr>
+				<th></th>
+				<th>KN</th>
+				<th>Habilitation contrôlées</th>
+				<th>SAMI</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr v-for="(row, rowIndex) in chartData.slice(1)" :key="rowIndex">
+				<td v-for="(value, columnIndex) in row" :key="columnIndex">
+					<template v-if="columnIndex === 3">
+						<StackedBar :bars="value" :value="true"></StackedBar>
+					</template>
+					<template v-else>
+						{{ value }}
+					</template>
+				</td>
+			</tr>
+		</tbody>
+	</table>
 </template>
   
 <script>
 import StackedBar from '../../components/pebble-ui/charts/StackedBar.vue'
-import { mapState } from 'vuex';
 export default {
-  data() {
-    return {
-      chartData: [],
-    }
-  },
-  props: {
-    requeteStat: {
-      type: Object,
-      required: true
-    }
-  },
-  computed: {
-    ...mapState(['personnels'])
-  },
-  methods: {
-    fetchData() {
-      this.chartData = [];
-      const data = this.$assets.getCollection('collectesCollection').getCollection();
-      let habilitationTypeHistory = [];
+	data() {
+		return {
+			chartData: [],
+		}
+	},
+	props: {
+		requeteStat: {
+			type: Object,
+			required: true
+		}
+	},
+	methods: {
+		fetchData() {
+			this.chartData = [];
+			let habilitationHistory = new Map();
+			const data = this.$assets.getCollection('collectesCollection').getCollection();
 
-      data.forEach(collecte => {
-        const id = collecte['personnel_id__operateur'];
-        const index = this.chartData.findIndex(operateur => (operateur[0] == 'Opérateur ' + id));
-        if (habilitationTypeHistory.findIndex(id => id == collecte['habilitation_type_id']) == -1) {
-          habilitationTypeHistory.push(collecte['habilitation_type_id']);
-        }
-        if (index >= 0) {
-          this.chartData[index][1]++;//incrémentation du champ KN
-          this.chartData[index][2] = habilitationTypeHistory.length;
-          switch (collecte['sami']) {
-            case 'S':
-              this.chartData[index][3][0].value++;
-              break;
-            case 'A':
-              this.chartData[index][3][1].value++;
-              break;
-            case 'M':
-              this.chartData[index][3][2].value++;
-              break;
-            case 'I':
-              this.chartData[index][3][3].value++;
-              break;
-            default:
-              this.chartData[index][3][4].value++;
-              break;
-          }
-        } else {
-          let newRow = ['Opérateur ' + id, 1, habilitationTypeHistory.length, [
-            {
-              color: 'success',
-              value: 0
-            },
-            {
-              color: 'primary',
-              value: 0
-            },
-            {
-              color: 'warning',
-              value: 0
-            },
-            {
-              color: 'danger',
-              value: 0
-            },
-            {
-              color: 'secondary',
-              value: 0
-            },
-          ]];
-          switch (collecte['sami']) {
-            case 'S':
-              newRow[3][0].value++;
-              break;
-            case 'A':
-              newRow[3][1].value++;
-              break;
-            case 'M':
-              newRow[3][2].value++;
-              break;
-            case 'I':
-              newRow[3][3].value++;
-              break;
-            default:
-              newRow[3][4].value++;
-              break;
-          }
-          this.chartData.push(newRow);
-        }
-      });
-      habilitationTypeHistory = [];
-    }
-  },
-  components: { StackedBar },
-  mounted() {
-    this.fetchData();
-  },
+			data.forEach(collecte => {
+				const id = collecte['personnel_id__operateur'];
+				const index = this.chartData.findIndex(operateur => (operateur[0] == 'Opérateur ' + id));
+				if (habilitationHistory.get(id) == null) {
+					habilitationHistory.set(id, []);
+				}
+				if (!habilitationHistory.get(id).includes(collecte['habilitation_id'])) {
+					habilitationHistory.get(id).push(collecte['habilitation_id']);
+				}
+				if (index >= 0) {
+					this.chartData[index][1]++;//incrémentation du champ KN
+					this.chartData[index][2] = habilitationHistory.get(id).length;
+					switch (collecte['sami']) {
+						case 'S':
+							this.chartData[index][3][0].value++;
+							break;
+						case 'A':
+							this.chartData[index][3][1].value++;
+							break;
+						case 'M':
+							this.chartData[index][3][2].value++;
+							break;
+						case 'I':
+							this.chartData[index][3][3].value++;
+							break;
+						default:
+							this.chartData[index][3][4].value++;
+							break;
+					}
+				} else {
+					let newRow = [this.getOperatorCacheNomById(id), 1, habilitationHistory.get(id).length, [
+						{
+							color: 'success',
+							value: 0
+						},
+						{
+							color: 'primary',
+							value: 0
+						},
+						{
+							color: 'warning',
+							value: 0
+						},
+						{
+							color: 'danger',
+							value: 0
+						},
+						{
+							color: 'secondary',
+							value: 0
+						},
+					]];
+					switch (collecte['sami']) {
+						case 'S':
+							newRow[3][0].value++;
+							break;
+						case 'A':
+							newRow[3][1].value++;
+							break;
+						case 'M':
+							newRow[3][2].value++;
+							break;
+						case 'I':
+							newRow[3][3].value++;
+							break;
+						default:
+							newRow[3][4].value++;
+							break;
+					}
+					this.chartData.push(newRow);
+				}
+			});
+		},
+		getOperatorCacheNomById(id) {
+			let personnels = this.$assets.getCollection('personnels').getCollection();
+			const personnel = personnels.find(e => e.id == id);
+			return personnel ? personnel.cache_nom : 'Opérateur (' + id + ') non trouvé'
+		}
+	},
+	components: { StackedBar },
+	mounted() {
+		this.fetchData();
+	},
 }
 </script>
   
