b875d077d091483063a3e0bb04ed730663422960
glesaout <gabin@pebble.bzh>
2023-08-03 15:03:10 +0200

    Modif label
    Modification de return label dans la progressbar et ajout props dans la vue

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,98 +1,111 @@
 <template>
-    <div v-if="varTime" class="progress progress-ht" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-        <div class="progress-bar progress-ht overflow-visible text-light" role="progressbar"    :class="returnClass(varTime)" :style="returnStyle(varTime)" >
+    <div v-if="varTime" class="progress progress-ht" role="progressbar" aria-label="Example with label" aria-valuenow="25"
+        aria-valuemin="0" aria-valuemax="100">
+        <div class="progress-bar progress-ht overflow-visible text-light" role="progressbar" :class="returnClass(varTime)"
+            :style="returnStyle(varTime)">
             {{ returnLabel(varTime) }}
         </div>
-    </div> 
+    </div>
     <!-- <div>{{ varTime }}</div> -->
 </template>
 
 <script>
 
 export default {
-    
+
     props: {
-        dd:{
+        dd: {
             type: String,
         },
         df: {
+            type: String,
+        },
+        label: {
             type: String,
         }
     },
     computed: {
 
-        varTime(){
+        varTime() {
             const now = new Date();
             const endDate = new Date(this.df);
             const beginDate = new Date(this.dd)
-            let nowTime = now.getTime()/1000;
-            let endTime = endDate.getTime()/1000;
-            let beginTime = beginDate.getTime()/1000;
+            let nowTime = now.getTime() / 1000;
+            let endTime = endDate.getTime() / 1000;
+            let beginTime = beginDate.getTime() / 1000;
 
             let consoTime = nowTime - beginTime;
             let restTime = endTime - nowTime;
             let totalTime = endTime - beginTime;
 
-            let restinDays = Math.floor(restTime / (60*60*24));
-            let totalDays = Math.floor(totalTime) /(60*60*24);
-            let consoDays = Math.floor(consoTime)/(60*24*60);
+            let restinDays = Math.floor(restTime / (60 * 60 * 24));
+            let totalDays = Math.floor(totalTime) / (60 * 60 * 24);
+            let consoDays = Math.floor(consoTime) / (60 * 24 * 60);
 
-            let consoPerCent = Math.ceil((consoDays/totalDays)*100)
+            let consoPerCent = Math.ceil((consoDays / totalDays) * 100)
 
-            let data = {restinDays,consoPerCent,totalDays}
-           
+            let data = { restinDays, consoPerCent, totalDays }
+
             return data
         },
-        percent(){
+        percent() {
             const now = new Date();
             const beginDate = new Date(this.dd);
 
-            let nowTime = now.getTime()/1000;
-            let beginTime = beginDate.getTime()/1000;
+            let nowTime = now.getTime() / 1000;
+            let beginTime = beginDate.getTime() / 1000;
 
-            let time = nowTime-beginTime;
-            let consoDays = Math.floor(time /(60*60*24));
-            let  percent = Math.floor(consoDays/ 11);
+            let time = nowTime - beginTime;
+            let consoDays = Math.floor(time / (60 * 60 * 24));
+            let percent = Math.floor(consoDays / 11);
             return percent
         }
 
     },
     methods: {
-        returnClass(data){  
-            if(50>=data.consoPerCent && data.consoPerCent>=0) {
+        returnClass(data) {
+            if (50 >= data.consoPerCent && data.consoPerCent >= 0) {
                 return 'bg-success'
             }
-            else if(70>=data.consoPerCent && data.consoPerCent>=51){
+            else if (70 >= data.consoPerCent && data.consoPerCent >= 51) {
                 return 'bg-info'
-            } 
-            else if(85>=data.consoPerCent && data.consoPerCent>=71){
+            }
+            else if (85 >= data.consoPerCent && data.consoPerCent >= 71) {
                 return 'bg-warning'
             }
-            else if(100>data.consoPerCent && data.consoPerCent>=86){
+            else if (100 > data.consoPerCent && data.consoPerCent >= 86) {
                 return 'bg-danger text-light'
             }
             else return 'bg-secondary text-light'
-            
+
         },
 
-        returnStyle(data){
-            if(data.consoPerCent>0){
-               return "width:"+data.consoPerCent +"%"
+        returnStyle(data) {
+            if (data.consoPerCent > 0) {
+                return "width:" + data.consoPerCent + "%"
             }
             else return "width: 100%"
         },
 
-        returnLabel(data){
-            if(data.restinDays <= 0) {
-                return 'expiré depuis '+Math.abs(data.restinDays)+' jours'
+        returnLabel(data) {
+            const label = this.label ? this.label : '';
+            if (data.restinDays <= 0) {
+                return this.cleanLabel(label + ' expiré depuis ') + Math.abs(data.restinDays) + ' jours';
             }
-            else{
-                if(data.consoPerCent >= 71){
-                    return 'reste '+data.restinDays+' jours'
+            else {
+                if (data.consoPerCent >= 71) {
+                    return this.cleanLabel(label + ' à réaliser sous ') + data.restinDays + ' jours'
                 }
-
             }
-        }
+        },
+        /**
+         * Enlève les espaces au début et à la fin puis ajoute une majuscule au début si nécessaire
+         * @param {String} label 
+         */
+        cleanLabel(label){
+            let lowerCaseLabel = label.slice(0);
+            return lowerCaseLabel.charAt(0).toUpperCase() + lowerCaseLabel.slice(1);
+        },
     }
 }
 
