dcab520128feb1d1fdbde41abd81a416f1bd4869
glesaout <gabin@pebble.bzh>
2023-08-08 13:50:20 +0200

    sami avec couleurs

--- src/components/habilitation/ControlForm.vue
+++ src/components/habilitation/ControlForm.vue
@@ -13,6 +13,25 @@
                 v-on:click="setMode('Non contrôlés')">
             <label class="btn btn-outline-secondary" for="filtreRetardNonControle">Non contrôlés</label>
         </div>
+
+        <div class="my-2 btn-group w-100">
+            <input type="radio" class="btn-check" name="filtreRetard" id="filtreRetardS" checked autocomplete="off"
+                v-on:click="setMode('S')">
+            <label class="btn btn-outline-success" for="filtreRetardS">S</label>
+
+            <input type="radio" class="btn-check" name="filtreRetard" id="filtreRetardA" autocomplete="off"
+                v-on:click="setMode('A')">
+            <label class="btn btn-outline-primary" for="filtreRetardA">A</label>
+
+            <input type="radio" class="btn-check" name="filtreRetard" id="filtreRetardM" autocomplete="off"
+                v-on:click="setMode('M')">
+            <label class="btn btn-outline-warning" for="filtreRetardM">M</label>
+
+            <input type="radio" class="btn-check" name="filtreRetard" id="filtreRetardI" autocomplete="off"
+                v-on:click="setMode('I')">
+            <label class="btn btn-outline-danger" for="filtreRetardI">I</label>
+        </div>
+
         <div class="text-center">
             <button class="btn btn-primary bi-check" type="button" @click.prevent="emitForm()">Appliquer</button>
         </div>
@@ -42,6 +61,19 @@ export default {
                 case 'Non contrôlés':
                     this.queryParameters.last_control_result = ''
                     break;
+                case 'S':
+                    this.queryParameters.last_control_result = 'S'
+                    break;
+                case 'A':
+                    this.queryParameters.last_control_result = 'A'
+                    break;
+                case 'M':
+                    this.queryParameters.last_control_result = 'M'
+                    break;
+                case 'I':
+                    this.queryParameters.last_control_result = 'I'
+                    break;
+
                 default:
                     break;
             }
