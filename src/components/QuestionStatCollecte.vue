43d93a1d3e315bf65eedd126758b995f0dd74e17
TigrouForMatic <jules.chassany@gmail.com>
2024-05-02 03:34:58 +0200

    Ajout d'un affichage différent en fonction du temps depuis le dernier controle de question

--- src/components/QuestionStatCollecte.vue
+++ src/components/QuestionStatCollecte.vue
@@ -1,43 +1,52 @@
 <template>
-   <div class="container bg-light rounded-3 my-1 border" v-if="stat">
-        <h5 class="ms-1 mt-2">Informations sur cette question : </h5>
-        <div class="row my-2">
-            <div class="col-1"></div>  
-            <div class="col-2 d-flex align-items-center" >
-                <div v-if="stat.dc">
-                    <span class="me-2">{{ dateStatFormat }}</span>
-                    <FormModuleSAMIHeader :value="stat.data_var" />
-                </div>
-                <div v-else>
-                    <p class="text-danger">"Non Contrôlé"</p>
-                </div>
-            </div>
-            <!-- <p>Barre de progression (FIS), avec la note moyenne individuelle et collective de chaque item.</p> -->
-            <div class="col-8 my-3">
-                <JaugeSami :personnalAverage="stat.cibleAverage" :name="cible_nom" :generalAverage="stat.globalAverage" />
-            </div>
-            <div class="col-1"></div>
-        </div>
-    </div>
-</template>
-
-<script>
-import FormModuleSAMIHeader from './form/FormModuleSAMIHeader.vue';
-import {dateFormat} from '../js/date';
-import JaugeSami from './JaugeSami.vue';
-
-export default {
-    components: { FormModuleSAMIHeader, JaugeSami },
-
-    props: {
-        stat: Object,
-        cible_nom: String
-    },
-
-    computed: {
-        dateStatFormat() {
-            return dateFormat(this.stat.dc)
-        }
-    }
-};
-</script>
+    <div class="container bg-light rounded-3 my-1 border" v-show="stat">
+         <h5 class="ms-1 mt-2">Informations sur cette question : </h5>
+         <div class="row my-2">
+             <div class="col-1"></div>  
+             <div class="col-2 d-flex align-items-center" >
+                 <div v-if="stat.dc">
+                     <span class="me-2" :class="dateClass">{{ dateFormatted }}</span>
+                     <FormModuleSAMIHeader :value="stat.data_var" />
+                 </div>
+                 <div v-else>
+                     <p class="text-danger">"Non Contrôlé"</p>
+                 </div>
+             </div>
+             <div class="col-8 my-3">
+                 <JaugeSami :personnalAverage="stat.cibleAverage" :name="cible_nom" :generalAverage="stat.globalAverage" />
+             </div>
+             <div class="col-1"></div>
+         </div>
+     </div>
+ </template>
+ 
+ <script>
+ import FormModuleSAMIHeader from './form/FormModuleSAMIHeader.vue';
+ import {dateFormat, diffDate} from '../js/date';
+ import JaugeSami from './JaugeSami.vue';
+ 
+ export default {
+     components: { FormModuleSAMIHeader, JaugeSami },
+ 
+     props: {
+         stat: Object,
+         cible_nom: String
+     },
+ 
+     computed: {
+         dateFormatted() {
+             return this.stat.dc ? dateFormat(this.stat.dc) : '';
+         },
+ 
+         dateClass() {
+             if (!this.stat.dc) return 'text-danger';
+             const weeksDiff = diffDate(new Date(this.stat.dc), new Date(), "week");
+             console.log(weeksDiff)
+             if (weeksDiff >= 52) return "text-danger";
+             else if (weeksDiff >= 26) return "text-warning";
+             else return "text-success";
+         }
+     }
+ };
+ </script>
+ 
