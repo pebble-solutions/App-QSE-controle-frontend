94f7c44c25f8422da4158fc513aa44b6d90e45db
TigrouForMatic <jules.chassany@gmail.com>
2024-05-06 11:21:23 +0200

    opti ergo, opti comp

--- src/views/ConsultationCollecteAddPrevious.vue
+++ src/views/ConsultationCollecteAddPrevious.vue
@@ -7,72 +7,23 @@
         :submitBtn="valueButton"
         :cancelBtn="true">
 
-        <div class="card my-2">
-        <div class="card-header container">
-            <div class="text-primary text-bg-light d-flex align-items-center row">
-                <div class="col-2"></div>
-                <i class="bi bi-calendar-check me-1 col-1"></i>
-                <span class="d-none d-sm-inline col-3">Date de contrôle</span>
-                <span>{{ getDisplayFormatedDate(collecte.date_start) }}</span>
-                <div class="col-2"></div>
-            </div>
-        </div>
-        <div class="card-body">
-            <div class="row">
-                <div class="mb-2 col">
-                    <div class="d-flex align-items-center">
-                        <user-image :name="collecte.cible_nom" />
-                        <span>{{ collecte.cible_nom }}</span>
-                    </div>
-                </div>
-                
-                <div class="mb-2 col border-start border-dark">
-                    <div class="d-flex align-items-center">
-                        <user-image :name="collecte.enqueteur_nom" />
-                        <span>{{ collecte.enqueteur_nom }}</span>
-                    </div>
+       <AddPreviousForm :collecte="collecte" :collectes="collectes" :parentPreviousId="previous_id" @modification="perviousIdChange" v-if="!pending.collectes"/>
+
+        <div v-if="noteContent">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Note de la collecte #{{ $route.params.idCollecte }}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Note justificative</h6>
+                    <p class="card-text">{{ noteContent }}</p>
                 </div>
             </div>
-        </div>
-    </div>
 
-    <div class="mb-3 container">
-        <div class="row">
-            <label class="form-label col-3 mt-1" for="collecteBouclage">Controle de Bouclage</label>
-            <select
-                class="form-select col w-50"
-                id="collecteCible"
-                name="cible_personnel"
-                v-model="previous_id"
-                ref="collecteCibleSelect"
-
-                >
-                <option
-                    v-for="col in collectes"
-                    :value="col.id"
-                    :key="'collecte-' + col.id"
-                >
-                    #{{ col.id }} : {{ getDisplayFormatedDate(col.date_done) }}
-                </option>
-            </select>
-        </div>
-    </div>
-
-    <div v-if="noteContent">
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Note de la collecte #{{ $route.params.idCollecte }}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">Note justificative</h6>
-                <p class="card-text">{{ noteContent }}</p>
+            <div class="mb-3 mt-3">
+                <label for="formComment" class="form-label fs-5 ms-2">Justification</label>
+                <i class="bi bi-exclamation-triangle text-warning ms-3" v-if="!comment"></i>
+                <textarea class="form-control" id="formComment" rows="3" placeholder="Veuillez entrez une justification..." v-model="comment" required></textarea>
             </div>
         </div>
-
-        <div class="mb-3 mt-3">
-            <label for="formComment" class="form-label fs-5 ms-2">Justification</label>
-            <i class="bi bi-exclamation-triangle text-warning ms-3" v-if="!comment"></i>
-            <textarea class="form-control" id="formComment" rows="3" placeholder="Veuillez entrez une justification..." v-model="comment" required></textarea>
-        </div>
-    </div>
 
     </AppModal>
 </template>
@@ -81,8 +32,7 @@
 
 import { mapState, mapActions } from 'vuex'
 import AppModal from '../components/pebble-ui/AppModal.vue'
-import UserImage from '../components/pebble-ui/UserImage.vue'
-import {dateFormat} from '../js/date'
+import AddPreviousForm from '../components/collecte/AddPreviousForm.vue'
 
 export default {
 
@@ -91,13 +41,10 @@ export default {
             collectes : [],
             previous_id: null,
             noteContent: null,
-            comment: null
-        }
-    },
-
-    watch: {
-        previous_id(){
-            this.createNote()
+            comment: null,
+            pending : {
+                collectes : false
+            }
         }
     },
 
@@ -147,17 +94,13 @@ export default {
         },
 
         /**
-         * Met a jour la valeur de la collecte modifié lors de la reception de l'evenement associé
+         * Met a jour la valeur du previous_id modifié lors de la reception de l'evenement associé
          * 
          * @param {Object} payload 
          */
-        collecteChange(payload) {
-            this.collecteModifie = payload;
+        perviousIdChange(payload) {
+            this.previous_id=payload;
             this.createNote()
-        },
-
-        getDisplayFormatedDate(date) {
-            return dateFormat(date)
         },
 
          /**
@@ -168,6 +111,8 @@ export default {
          * @return {object}
          */
          async getCollectes(){
+
+            this.pending.collectes = true;
 
             let options = {
                 personnel_id__operateur: this.collecte.cible__structure__personnel_id,
@@ -187,6 +132,7 @@ export default {
             finally {
                 this.collectes = tmpCollectes.reverse();
                 this.previous_id = this.collectes[0].id;
+                this.pending.collectes = false;
             }
         },
 
@@ -202,7 +148,7 @@ export default {
         this.getCollectes();
     },
 
-    components: { AppModal, UserImage }
+    components: { AppModal, AddPreviousForm }
 }
 
 </script>
